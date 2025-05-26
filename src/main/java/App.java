293f484ddbb05856644b01e35954b69c673ef0ee
aayush <aayush@k.com>
2025-05-26 13:36:31 +0530

    added app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("CMDB Application Started");
+        System.out.println("Hello, DevOps World!");
     }
-} 
+}
